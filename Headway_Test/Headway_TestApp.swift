f0834920ba6f7e59315eb2012b6fc6f7d0345da5
Robert Koval <kovalrobert@me.com>
2023-11-20 16:09:08 +0200

    Initial Commit

---
+++ Headway_Test/Headway_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Headway_TestApp.swift
+//  Headway_Test
+//
+//  Created by Robert Koval on 20.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct Headway_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
